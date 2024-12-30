df51bb47f737c52afe8c7c0a81baaf650f26c263
Iker Cubillo <i.cubillo@opendeusto.es>
2024-12-30 12:45:58 +0100

    tank head rotation

--- Assets/Z-SuperTank/Scripts/SuperTank.cs
+++ Assets/Z-SuperTank/Scripts/SuperTank.cs
@@ -11,6 +11,7 @@ public class SuperTank : Agent
     // [SerializeField] private Material winMaterial;
     // [SerializeField] private Material loseMaterial;
     // [SerializeField] private MeshRenderer floorMeshRenderer;
+    [SerializeField] private Transform tankHead;
     public override void OnEpisodeBegin()
     {
         // transform.localPosition = Vector3.zero;
@@ -31,21 +32,33 @@ public class SuperTank : Agent
     {        
         float rotate = actions.ContinuousActions[0]; // Rotación
         float moveZ = actions.ContinuousActions[1]; // Movimiento hacia adelante/atrás
+        float headRotate = actions.ContinuousActions[2];
 
         float moveSpeed = 3f;
         float rotationSpeed = 100f;
+        float headRotationSpeed = 50f;
 
         // Movimiento hacia adelante o atrás
         transform.localPosition += transform.forward * moveZ * Time.deltaTime * moveSpeed;
 
         // Rotación
         transform.Rotate(Vector3.up, rotate * Time.deltaTime * rotationSpeed);
+
+        // Girar la cabeza del tanque
+        tankHead.Rotate(Vector3.up, headRotate * Time.deltaTime * headRotationSpeed);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
-        continousActions[0] = Input.GetAxisRaw("Horizontal");
-        continousActions[1] = Input.GetAxisRaw("Vertical");
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+        // Movimiento de la cabeza (Q y E para girar)
+        if (Input.GetKey(KeyCode.Q))
+            continuousActions[2] = -1f; // Girar la cabeza a la izquierda
+        else if (Input.GetKey(KeyCode.E))
+            continuousActions[2] = 1f; // Girar la cabeza a la derecha
+        else
+            continuousActions[2] = 0f; // Sin rotación
     }
 }
